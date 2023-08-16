3f51db0005fdb383f4e4627ad030d953396dd656
elekpow <46938469+elekpow@users.noreply.github.com>
2023-08-16 12:36:52 +0300

    PRIVILEGES

--- reldb/lesson6/files/set-permission.sql
+++ reldb/lesson6/files/set-permission.sql
@@ -1,9 +1,5 @@
-USE mysql;
-CREATE USER 'replication'@'%';
-CREATE USER 'replication'@'localhost';
-ALTER USER 'replication' IDENTIFIED WITH mysql_native_password BY 'Pass12345';
-
-GRANT ALL PRIVILEGES ON *.* TO 'replication'@'%';
-FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES ON *.* TO 'replication'@'localhost';
-FLUSH PRIVILEGES;
+ USE mysql;
+ CREATE USER 'replication'@'%';
+ ALTER USER 'replication' IDENTIFIED WITH mysql_native_password BY 'Pass12345';
+ GRANT REPLICATION SLAVE ON *.* TO 'replication'@'%';
+ FLUSH PRIVILEGES;
